53ca7320c91a4022b13d4063f3fe27ad7780608b
strea <stream.larn3@gmail.com>
2019-09-21 00:10:27 +0900

    main.cpp移行

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID "cbdad163-7a4a-4908-a257-219b571bb663"
+#define CHARACTERISTIC_UUID "cfea1bbb-7cb3-4c95-a52a-2276b74839e5"
+
+bool stateChanged = false;
+
+class MyCallbacks : public BLECharacteristicCallbacks
+{
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
+        String value = String(pCharacteristic->getValue().c_str());
+        Serial.println(value);
+    }
+};
+
+void setup()
+{
+    Serial.begin(115200); // Status message will be sent to PC at 9600 baud
+
+    BLEDevice::init("S-T-R-I-V-E Reactor");
+    BLEServer *pServer = BLEDevice::createServer();
+    BLEService *pService = pServer->createService(SERVICE_UUID);
+    BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID,
+        BLECharacteristic::PROPERTY_READ |
+            BLECharacteristic::PROPERTY_WRITE);
+    pCharacteristic->setCallbacks(new MyCallbacks());
+    pCharacteristic->setValue("N");
+    pService->start();
+    BLEAdvertising *pAdvertising = pServer->getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->start();
+}
+
+//+=============================================================================
+// The repeating section of the code
+//
+void loop()
+{
+    delay(2000);
+}
